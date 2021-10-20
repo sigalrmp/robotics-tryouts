66956595ba24247dcc9394e75c3eecd7ce6ddc44
Sigal Minsky-Primus <snigbot@gmail.com>
2021-10-20 18:17:16 -0400

    _

---
+++ Taylor.java
@@ -0,0 +1,20 @@
+public class Taylor {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static double TaylorE(double x, int n) {
+        if (n == 0) {
+            return 1;
+        }
+        else return Math.pow(x, n) / factorial(n) + TaylorE(x, n - 1);
+    }
+
+    public static int factorial(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        else return n * factorial(n - 1);
+    }
+}
